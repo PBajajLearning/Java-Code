486c362f3f53309681f2e1cdbf523d60b8cab6d3
PBajajLearning <bajaj.puja@gmail.com>
2021-11-24 23:45:41 +0000

    TwoSum

--- src/com/learning/arrays/TwoSum.java
+++ src/com/learning/arrays/TwoSum.java
@@ -1,2 +1,40 @@
-package com.learning.arrays;public class TwoSum {
+package com.learning.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+
+        Map<Integer, Integer> store = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+            int diff = target - curr;
+            Integer idx = store.get(diff);
+
+            if (idx != null) {
+                return new int[]{i, idx};
+
+            } else {
+                store.put(curr, i);
+            }
+
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,7,11,70};
+        int target = 13;
+        int[] result = twoSum(arr,target);
+        System.out.println(Arrays.toString(result));
+    }
+
 }
+
